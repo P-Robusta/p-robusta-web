5ed71664a4bbc8517bf4f90d4d79bfff3cab717a
cunguyendev <cunguyen.dev@gmail.com>
2021-07-11 01:08:26 +0700

    Force from old project.

---
+++ README.md
@@ -0,0 +1,50 @@
+# Landing page with ReactJS
+
+A landing page build from ReactJS
+
+Deploy to: (Updating...)
+
+## Developers
+
+- Cu Nguyen &lt;[cunguyen.dev@gmail.com](cnguyen.dev@gmail.com)&gt;
+
+## Requirements
+
+- [Node](https://nodejs.org/en/) &gt;=v14.x.x / [npm](https://www.npmjs.com/) &gt;=7.x.x
+  - Recommend installing with [nvm](https://github.com/creationix/nvm)
+- [Yarn](https://classic.yarnpkg.com) &gt;=1.22.4
+
+## Tech stacks
+
+- HTML/CSS/JavaScript
+- ReactJS
+
+## Getting Started
+
+Step by step to run this app in your local
+
+### Install packages
+
+At your directory root, run:
+
+```
+npm/yarn install
+```
+
+### Run server
+
+```
+npm/yarn start
+```
+
+Open on web: http://localhost:3003 (this is by default, you can custom it)
+
+### Build
+
+```
+npm/yarn run build
+```
+
+_You can update the source structures to follow your patterns._
+
+_Note: Live-reload is supported_
---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react'
+
+import { Navigation } from './components/navigation'
+import { Header } from './components/header'
+import { Features } from './components/features'
+import { About } from './components/about'
+import { Services } from './components/services'
+import { Gallery } from './components/gallery'
+import { Testimonials } from './components/testimonials'
+import { Team } from './components/Team'
+import { Contact } from './components/contact'
+import appData from './data/data.json'
+import SmoothScroll from 'smooth-scroll'
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+})
+
+const App = () => {
+  const [landingPageData, setLandingPageData] = useState({})
+
+  /**
+   * This app is using the hard data
+   * We can use data from any APIs by custom it
+   */
+  useEffect(() => {
+    setLandingPageData(appData)
+  }, [])
+
+  return (
+    <div>
+      <Navigation />
+      <Header data={landingPageData.Header} />
+      <Features data={landingPageData.Features} />
+      <About data={landingPageData.About} />
+      <Services data={landingPageData.Services} />
+      <Gallery />
+      <Testimonials data={landingPageData.Testimonials} />
+      <Team data={landingPageData.Team} />
+      <Contact data={landingPageData.Contact} />
+    </div>
+  )
+}
+
+export default App
