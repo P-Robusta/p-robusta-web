c75878e11a1d2794a07790006bb8f7d38f358845
huynguyenngocdev <ngochuy2k1vinhdinh@gmail.com>
2021-07-16 17:00:40 +0700

    Complete helper CallAPI.

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,38 @@
 // Dev dependencies
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 // Components
-import { Navigation } from './components/navigation'
-import { Header } from './components/header'
-import { Features } from './components/features'
-import { About } from './components/about'
-import { Services } from './components/services'
-import { Gallery } from './components/gallery'
-import { Testimonials } from './components/testimonials'
-import { Team } from './components/Team'
-import { Contact } from './components/contact'
+import { Navigation } from './components/Navigation/navigation';
+import { Header } from './components/Header/header';
+import { Features } from './components/features';
+import { About } from './components/About/about';
+import { Services } from './components/services';
+import { Gallery } from './components/gallery';
+import { Testimonials } from './components/Testimonials/testimonials';
+import { Team } from './components/Staff/Team';
+import { Contact } from './components/Contact/contact';
 
-import CallAPI from './api/callAPI'
+import CallAPI from './helper/callAPI';
 // fake data
-import appData from './data/data.json'
+import appData from './data/data.json';
 // convinient module
-import SmoothScroll from 'smooth-scroll'
+import SmoothScroll from 'smooth-scroll';
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
   speedAsDuration: true,
-})
+});
 
 const App = () => {
-  const [landingPageData, setLandingPageData] = useState({})
+  const [landingPageData, setLandingPageData] = useState({});
 
   /**
    * This app is using the hard data
    * We can use data from any APIs by custom it
    */
   useEffect(() => {
-    setLandingPageData(appData)
-  }, [])
+    setLandingPageData(appData);
+    CallAPI('categories').then((res) => console.log(res.data.data));
+  }, []);
 
   return (
     <div>
@@ -44,9 +45,8 @@ const App = () => {
       <Testimonials data={landingPageData.Testimonials} />
       <Team data={landingPageData.Team} />
       <Contact data={landingPageData.Contact} />
-      <CallAPI/>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/helper/callAPI.jsx
@@ -0,0 +1,76 @@
+import axios from 'axios';
+const CallAPI = async (endpoit) => {
+  // ----------------Set URL----------------
+  // const api_url = process.env.REACT_APP_API_URL
+  const api_url = 'http://localhost:8000/api/client';
+
+  let inforToken = JSON.parse(localStorage.getItem('__client_token_pnv__'));
+  let expirationDate = Number(localStorage.getItem('__day_get_token_pnv__'));
+  // ----------------Get token function----------------
+  const getToken = async () => {
+    //set base infor
+    // const getTokenUrl = process.env.REACT_APP_API_URL_GET_TOKEN
+    const getTokenUrl = 'http://localhost:8000/oauth/token';
+
+    let client_oauth = new URLSearchParams();
+    client_oauth.append('client_id', process.env.REACT_APP_CLIENT_ID);
+    client_oauth.append('client_secret', process.env.REACT_APP_CLIENT_SECRET);
+    client_oauth.append('grant_type', process.env.REACT_APP_GRANT_TYPE);
+
+    const configs = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Accept: 'application/json, text/plain, */*',
+      },
+    };
+    //call oauth to get access_token
+    await axios
+      .post(getTokenUrl, client_oauth, configs)
+      .then((res) => {
+        localStorage.setItem('__client_token_pnv__', JSON.stringify(res.data));
+        localStorage.setItem('__day_get_token_pnv__', new Date().getTime());
+      })
+      .catch((err) => console.log(err));
+    checkToken();
+  };
+
+  //----------------check token, if it have -> pass, else -> get token----------------
+  const checkToken = async () => {
+    let dateNow = new Date().getTime();
+
+    if (
+      inforToken &&
+      inforToken.access_token &&
+      expirationDate &&
+      Math.abs((dateNow - expirationDate) / 1000) < inforToken.expires_in
+    ) {
+      //continue check connet api
+      await axios
+        .get(`${api_url}/categories`, {
+          headers: {
+            Authorization: `${inforToken.token_type} ${inforToken.access_token}`,
+            Accept: 'application/json',
+          },
+        })
+        .catch(async (error) => {
+          console.error(error);
+          if (error.message.indexOf('401') > -1) {
+            await getToken();
+            window.location.reload();
+          }
+        });
+    } else {
+      await getToken();
+    }
+  };
+  await checkToken();
+  //----------------call api----------------
+  return axios.get(`${api_url}/${endpoit}`, {
+    headers: {
+      Authorization: `${inforToken.token_type} ${inforToken.access_token}`,
+      Accept: 'application/json',
+    },
+  });
+};
+
+export default CallAPI;
